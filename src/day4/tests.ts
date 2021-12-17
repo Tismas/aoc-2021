cdd062fb692126854470e83f1cb60bb36253df49
Adam Golan <adam.golan@allegro.pl>
2021-12-17 14:20:13 +0100

    initial state

---
+++ src/day4/tests.ts
@@ -0,0 +1,10 @@
+import { part1, part2 } from "./solution";
+
+describe("day1", () => {
+  it("part1", () => {
+    expect(part1("")).toEqual("");
+  });
+  it("part2", () => {
+    expect(part2("")).toEqual("");
+  });
+});
