de6cb5f653d63ce640dae02b6a2bbd0127a3f1a2
Adam Golan <adam.golan@allegro.pl>
2021-12-17 15:05:12 +0100

    day1

--- src/day1/solution.ts
+++ src/day1/solution.ts
@@ -1,7 +1,37 @@
+const windowIncreased = (
+  arr: number[],
+  lastWindowIndex: number,
+  window = 1
+) => {
+  if (lastWindowIndex <= window) return false;
+
+  let sum = 0;
+  let prevSum = 0;
+  for (let i = lastWindowIndex - window; i < lastWindowIndex; i++) {
+    sum += arr[i];
+    prevSum += arr[i - 1];
+  }
+
+  return sum > prevSum;
+};
+
 export const part1 = (input: string) => {
-  return input;
+  return input
+    .split("\n")
+    .map(Number)
+    .reduce((acc, val, i, arr) => {
+      if (windowIncreased(arr, i + 1)) return acc + 1;
+      return acc;
+    }, 0);
 };
 
 export const part2 = (input: string) => {
-  return input;
+  const window = 3;
+  return input
+    .split("\n")
+    .map(Number)
+    .reduce((acc, val, i, arr) => {
+      if (windowIncreased(arr, i + 1, window)) return acc + 1;
+      return acc;
+    }, 0);
 };
--- src/day1/tests.ts
+++ src/day1/tests.ts
@@ -1,10 +1,21 @@
 import { part1, part2 } from "./solution";
 
+const input = `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`;
+
 describe("day1", () => {
   it("part1", () => {
-    expect(part1("")).toEqual("");
+    expect(part1(input)).toEqual(7);
   });
   it("part2", () => {
-    expect(part2("")).toEqual("");
+    expect(part2(input)).toEqual(5);
   });
 });
