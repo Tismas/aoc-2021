5de91aaaf2b358f5114fd6a47d0999b63b7f6c34
Adam Golan <adam.golan@allegro.pl>
2021-12-17 15:58:38 +0100

    day3

--- src/day3/solution.ts
+++ src/day3/solution.ts
@@ -1,7 +1,63 @@
+const getMostCommonBitAtPosition = (numbers: string[], i: number): string => {
+  let numberOfZeros = 0;
+  for (const num of numbers) {
+    if (num[i] === "0") {
+      numberOfZeros += 1;
+    }
+  }
+  return numberOfZeros > numbers.length / 2 ? "0" : "1";
+};
+
+const getMostCommonBits = (numbers: string[]): string => {
+  let gamma = "";
+  for (let i = 0; i < numbers[0].length; i++) {
+    gamma += getMostCommonBitAtPosition(numbers, i);
+  }
+  return gamma;
+};
+
+const filterBySomething = (
+  numbers: string[],
+  predicate: (digit: string, mostCommon: string) => boolean
+): string => {
+  let pointer = 0;
+  while (numbers.length > 1) {
+    const mostCommonBit = getMostCommonBitAtPosition(numbers, pointer);
+    const numbersToRemove: string[] = [];
+    for (let i = 0; i < numbers.length; i++) {
+      if (!predicate(numbers[i][pointer], mostCommonBit)) {
+        numbersToRemove.push(numbers[i]);
+      }
+    }
+    numbers = numbers.filter((num) => !numbersToRemove.includes(num));
+    pointer = (pointer + 1) % numbers[0].length;
+  }
+  return numbers[0];
+};
+
 export const part1 = (input: string) => {
-  return input;
+  const numbers = input.split("\n");
+  let gamma = getMostCommonBits(numbers);
+
+  const epsilon = gamma
+    .split("")
+    .map((digit) => (digit === "0" ? "1" : "0"))
+    .join("");
+
+  return parseInt(gamma, 2) * parseInt(epsilon, 2);
 };
 
 export const part2 = (input: string) => {
-  return input;
+  let numbers = input.split("\n");
+
+  const oxygen = filterBySomething(
+    numbers,
+    (digit, mostCommon) => digit === mostCommon
+  );
+  const co2 = filterBySomething(
+    numbers,
+    (digit, mostCommon) => digit !== mostCommon
+  );
+
+  return parseInt(oxygen, 2) * parseInt(co2, 2);
 };
--- src/day3/tests.ts
+++ src/day3/tests.ts
@@ -1,10 +1,23 @@
 import { part1, part2 } from "./solution";
 
+const input = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`;
+
 describe("day1", () => {
   it("part1", () => {
-    expect(part1("")).toEqual("");
+    expect(part1(input)).toEqual(198);
   });
   it("part2", () => {
-    expect(part2("")).toEqual("");
+    expect(part2(input)).toEqual(230);
   });
 });
