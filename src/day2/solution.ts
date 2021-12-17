02bcc1c9e41828596b41705ca11c2a7d37ae2e4a
Adam Golan <adam.golan@allegro.pl>
2021-12-17 15:09:21 +0100

    day2

--- src/day2/solution.ts
+++ src/day2/solution.ts
@@ -1,7 +1,41 @@
 export const part1 = (input: string) => {
-  return input;
+  let x = 0,
+    y = 0;
+  for (const instruction of input.split("\n")) {
+    const [command, valueStr] = instruction.split(" ");
+    const value = +valueStr;
+
+    if (command === "forward") {
+      x += value;
+    } else if (command === "down") {
+      y += value;
+    } else if (command === "up") {
+      y -= value;
+    } else {
+      throw new Error("invalid command");
+    }
+  }
+  return x * y;
 };
 
 export const part2 = (input: string) => {
-  return input;
+  let x = 0,
+    y = 0,
+    aim = 0;
+  for (const instruction of input.split("\n")) {
+    const [command, valueStr] = instruction.split(" ");
+    const value = +valueStr;
+
+    if (command === "forward") {
+      x += value;
+      y += value * aim;
+    } else if (command === "down") {
+      aim += value;
+    } else if (command === "up") {
+      aim -= value;
+    } else {
+      throw new Error("invalid command");
+    }
+  }
+  return x * y;
 };
--- src/day2/tests.ts
+++ src/day2/tests.ts
@@ -1,10 +1,17 @@
 import { part1, part2 } from "./solution";
 
+const input = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`;
+
 describe("day1", () => {
   it("part1", () => {
-    expect(part1("")).toEqual("");
+    expect(part1(input)).toEqual(150);
   });
   it("part2", () => {
-    expect(part2("")).toEqual("");
+    expect(part2(input)).toEqual(900);
   });
 });
