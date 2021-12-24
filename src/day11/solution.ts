51fd2e9e1f740c9247b8cd82f2358a07f25817f9
Tismas <adamski256@gmail.com>
2021-12-24 11:59:25 +0100

    day 11

--- src/day11/solution.ts
+++ src/day11/solution.ts
@@ -1,7 +1,67 @@
+import { getNeighbours } from "../framework/2d";
+
+interface Octopus {
+  flashed: boolean;
+  value: number;
+}
+
+const parseInput = (input: string): Octopus[][] => {
+  return input.split(/\r?\n/).map((line) => Array.from(line).map((v) => ({ flashed: false, value: Number(v) })));
+};
+
+const tick = (grid: Octopus[][]): number => {
+  let flashesThisTick = 0;
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      grid[y][x].value += 1;
+    }
+  }
+  let flashed = grid.flat().some((octo) => octo.value > 9);
+  while (flashed) {
+    flashed = false;
+    for (let y = 0; y < grid.length; y++) {
+      for (let x = 0; x < grid[y].length; x++) {
+        const octopus = grid[y][x];
+        if (!octopus.flashed && octopus.value > 9) {
+          octopus.flashed = true;
+          flashesThisTick += 1;
+          flashed = true;
+          const neighbours = getNeighbours(grid, x, y, null, true);
+          Object.values(neighbours)
+            .filter((octo): octo is Octopus => octo)
+            .forEach((octo) => (octo.value += 1));
+        }
+      }
+    }
+  }
+  grid.flat().forEach((octo) => {
+    if (octo.value > 9) octo.value = 0;
+    octo.flashed = false;
+  });
+
+  return flashesThisTick;
+};
+
 export const part1 = (input: string) => {
-  return 0;
+  const grid = parseInput(input);
+  const iterations = 100;
+  let flashes = 0;
+
+  for (let i = 0; i < iterations; i++) {
+    flashes += tick(grid);
+  }
+
+  return flashes;
 };
 
 export const part2 = (input: string) => {
-  return 0;
+  const grid = parseInput(input);
+  let lastFlashes = 0;
+  let iteration = 0;
+
+  for (; lastFlashes < 100; iteration++) {
+    lastFlashes = tick(grid);
+  }
+
+  return iteration;
 };
--- src/day11/tests.ts
+++ src/day11/tests.ts
@@ -1,12 +1,21 @@
 import { part1, part2 } from "./solution";
 
-const input = ``;
+const input = `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`;
 
 describe("day11", () => {
   it("part1", () => {
-    expect(part1(input)).toEqual(0);
+    expect(part1(input)).toEqual(1656);
   });
   it("part2", () => {
-    expect(part2(input)).toEqual(0);
+    expect(part2(input)).toEqual(195);
   });
 });
